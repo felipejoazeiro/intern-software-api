98cd8987a90a210ad90be411d71b32c2bdf87ed3
felipe.joazeiro <felipe.joazeiro@ictcondor.com.br>
2024-06-04 12:55:00 -0300

    Construtores finalizados

--- Codigo_Fonte/src/main/java/br/com/tekno/features/employees/endpoint/EmployeeController.java
+++ Codigo_Fonte/src/main/java/br/com/tekno/features/employees/endpoint/EmployeeController.java
@@ -8,6 +8,7 @@ import java.sql.ResultSet;
 import br.com.tekno.features.employees.domain.entity.EmployeeEntity;
 import br.com.tekno.features.employees.domain.errors.EmployeeErrors;
 import br.com.tekno.features.employees.domain.use_case.EmployeeUsecase;
+import io.netty.handler.codec.http.HttpContentEncoder.Result;
 import io.quarkus.vertx.web.Body;
 import io.quarkus.vertx.web.Route;
 
@@ -18,5 +19,21 @@ public class EmployeeController {
     public ResultSet newEmployee(@Body EmployeeEntity dados) throws EmployeeErrors{
         return new EmployeeUsecase().newEmployee(dados);
     }
+
+    @Route(path = "editEmployee", methods=Route.HttpMethod.POST)
+    @Produces(MediaType.APPLICATION_JSON)
+    public ResultSet editEmployee(@Body EmployeeEntity dados){
+        
+    }
+    
+    @Route(path = "editContact", methods=Route.HttpMethod.POST)
+
+    @Route(path = "editDocuments", methods=Route.HttpMethod.POST)
+
+    @Route(path = "editTicket", methods=Route.HttpMethod.POST)
+
+    @Route(path = "editAccess", methods=Route.HttpMethod.POST)
+
+
 
 }
