d78f28c2d402c350a04b86e58c1c105051b2ddfc
felipe.joazeiro <felipe.joazeiro@ictcondor.com.br>
2024-06-04 13:19:04 -0300

    Inicio da criação do controle com intuito de editar informações

--- Codigo_Fonte/src/main/java/br/com/tekno/features/employees/endpoint/EmployeeController.java
+++ Codigo_Fonte/src/main/java/br/com/tekno/features/employees/endpoint/EmployeeController.java
@@ -8,7 +8,6 @@ import java.sql.ResultSet;
 import br.com.tekno.features.employees.domain.entity.EmployeeEntity;
 import br.com.tekno.features.employees.domain.errors.EmployeeErrors;
 import br.com.tekno.features.employees.domain.use_case.EmployeeUsecase;
-import io.netty.handler.codec.http.HttpContentEncoder.Result;
 import io.quarkus.vertx.web.Body;
 import io.quarkus.vertx.web.Route;
 
@@ -22,18 +21,33 @@ public class EmployeeController {
 
     @Route(path = "editEmployee", methods=Route.HttpMethod.POST)
     @Produces(MediaType.APPLICATION_JSON)
-    public ResultSet editEmployee(@Body EmployeeEntity dados){
-        
+    public ResultSet editEmployee(@Body EmployeeEntity dados) throws EmployeeErrors{
+        throw new EmployeeErrors("Algo");
     }
-    
+
     @Route(path = "editContact", methods=Route.HttpMethod.POST)
+    @Produces(MediaType.APPLICATION_JSON)
+    public ResultSet editContact(@Body EmployeeEntity dados) throws EmployeeErrors{
+        throw new EmployeeErrors("Algo");
+    }
 
     @Route(path = "editDocuments", methods=Route.HttpMethod.POST)
+    @Produces(MediaType.APPLICATION_JSON)
+    public ResultSet editDocuments(@Body EmployeeEntity dados) throws EmployeeErrors{
+        throw new EmployeeErrors("Algo");
+    }
 
     @Route(path = "editTicket", methods=Route.HttpMethod.POST)
+    @Produces(MediaType.APPLICATION_JSON)
+    public ResultSet editTicket(@Body EmployeeEntity dados) throws EmployeeErrors{
+        throw new EmployeeErrors("Algo");
+
+    }
 
     @Route(path = "editAccess", methods=Route.HttpMethod.POST)
-
-
+    @Produces(MediaType.APPLICATION_JSON)
+    public ResultSet editAccess(@Body EmployeeEntity dados) throws EmployeeErrors{
+        throw new EmployeeErrors("Algo");
+    }
 
 }
